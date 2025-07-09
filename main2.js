1bb148e346ee5b5520794590fe48dfc0b4612009
ArtemHarrison <zobovartem8@gmail.com>
2025-07-09 16:45:08 +0300

    Hallo

--- main2.js
+++ main2.js
@@ -3,41 +3,41 @@ const carsData = [
     type: "sedan",
     model: "BMW M4",
     km: 75000,
-    price: 80000,
-    img: "https://hips.hearstapps.com/hmg-prod/images/2025-bmw-m4-coupe-front-three-quarters-motion-65b935ea5dde6.jpg",
-    link: "https://auto.ria.com/uk/search/?indexName=auto&brand.id[0]=9&model.id[0]=2030" 
+    price: 53900,
+    img: "https://cdn1.riastatic.com/photosnew/auto/photo/bmw_m4__596253261fx.webp",
+    link: "https://auto.ria.com/uk/auto_bmw_m4_38183276.html" 
   },
   {
     type: "suv",
     model: "Mercedes-Maybach GLS",
     km: 100000,
-    price: 90000,
+    price: 76777,
     img: "https://hips.hearstapps.com/hmg-prod/images/2025-mercedes-maybach-gls-600-140-684896af6131c.jpg",
-    link: "https://auto.ria.com/uk/search/?indexName=auto&brand.id[0]=48&model.id[0]=2084"
+    link: "https://auto.ria.com/uk/auto_mercedes-benz_gls-class_38151585.html"
   },
   {
     type: "sedan",
     model: "Toyota Camry",
     km: 200000,
-    price: 20000,
+    price: 42596,
     img: "https://scene7.toyota.eu/is/image/toyotaeurope/CAM_03a_24:Large-Landscape?ts=1730888748169&resMode=sharp2",
-    link: "https://auto.ria.com/uk/search/?indexName=auto&brand.id[0]=79&model.id[0]=1426" 
+    link: "https://auto.ria.com/uk/newauto/auto-toyota-camry-1999934.html" 
   },
   {
     type: "hatchback",
     model: "Volkswagen Golf",
     km: 120000,
-    price: 12000,
+    price: 30050,
     img: "https://hips.hearstapps.com/hmg-prod/images/06-2025-golf-gti-autobahn-kings-red-metallic-30-680b97f9352d2.jpg",
-    link: "https://auto.ria.com/uk/search/?indexName=auto&brand.id[0]=84&model.id[0]=1524" 
+    link: "https://auto.ria.com/uk/newauto/auto-volkswagen-golf-2019161.html" 
   },
   {
     type: "suv",
     model: "Audi Q7",
     km: 85000,
-    price: 45000,
+    price: 30800,
     img: "https://hips.hearstapps.com/hmg-prod/images/2025-audi-q7-102-66edd2912544c.jpg",
-    link: "https://auto.ria.com/uk/search/?indexName=auto&brand.id[0]=4&model.id[0]=913" 
+    link: "https://auto.ria.com/uk/auto_audi_q7_38358176.html" 
   }
 ];
 
